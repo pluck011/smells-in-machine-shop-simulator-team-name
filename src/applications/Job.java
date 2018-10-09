9f6fcec84f1ded14452a2898016eafb0e8bcaaec
menne019 <menne019@morris.umn.edu>
2018-10-08 23:33:41 -0500

    Remove inner Task class #7
    As we use a Pair<> data structure we can get rid of the unused Task class and reduce the total number of 'data classes'

--- src/applications/Job.java
+++ src/applications/Job.java
@@ -71,25 +71,4 @@ class Job {
     public int getId() {
         return id;
     }
-
-    // top-level nested classes
-    private class Task {
-        // data members
-        private int machine;
-        private int time;
-
-        // constructor
-        Task(int theMachine, int theTime) {
-            machine = theMachine;
-            time = theTime;
-        }
-
-        public int getMachine() {
-            return machine;
-        }
-
-        public int getTime() {
-            return time;
-        }
-    }
 }
