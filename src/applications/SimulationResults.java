fbd7c0214dc395a9f35f9c24e4282d474d0588ec
Nick Plucker <pluck011@morris.umn.edu>
2018-10-11 16:07:31 -0500

    Remove inner JobCompletionData class #6
    JobCompletionData was removed as an inner class since a Map now replaces it. See commit [004b53197c07e257b73a2fcfdee4b9a382ecf8a3] for more info on the Map change.

--- src/applications/SimulationResults.java
+++ src/applications/SimulationResults.java
@@ -77,28 +77,4 @@ public class SimulationResults {
         jobCompletions[nextJob] = jobCompletionData;
         nextJob++;
     }
-
-    public static class JobCompletionData {
-        private final int completionTime;
-        private final int totalWaitTime;
-        private final int jobNumber;
-
-        public JobCompletionData(int jobNumber, int completionTime, int totalWaitTime) {
-            this.jobNumber = jobNumber;
-            this.completionTime = completionTime;
-            this.totalWaitTime = totalWaitTime;
-        }
-
-        public int getCompletionTime() {
-            return completionTime;
-        }
-
-        public int getTotalWaitTime() {
-            return totalWaitTime;
-        }
-
-        public int getJobNumber() {
-            return jobNumber;
-        }
-    }
 }
